8b921dbd180a89558dc2ef407743ba2c10122169
Abdo Nejjari <mr.nejjari.abdelhafid@gmail.com>
2022-11-18 10:34:38 +0100

    Add Header and Categories components

---
+++ src/Components/Categories.js
@@ -0,0 +1,16 @@
+/* eslint-disable quotes */
+import React from "react";
+import Header from "./Header";
+import Button from "./Shared/Button";
+
+const Categories = () => (
+  <>
+    <Header />
+    <div className="book-categories">
+      <h2>Categories!</h2>
+      <Button btnName="category-btn" btnValue="Check Status" />
+    </div>
+  </>
+);
+
+export default Categories;
