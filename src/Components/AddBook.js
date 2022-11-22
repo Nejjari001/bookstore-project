0d5ba45582a89067291883c490d03d91f9e43dbb
Abdo Nejjari <mr.nejjari.abdelhafid@gmail.com>
2022-11-22 09:58:50 +0100

    Updates Components files

--- src/Components/AddBook.js
+++ src/Components/AddBook.js
@@ -4,8 +4,18 @@ import Button from './Shared/Button';
 const AddBook = () => (
   <div className="add-book">
     <form>
-      <input type="text" name="title" placeholder="Enter Book Title" required />
-      <input type="text" name="author" placeholder="Enter Author" required />
+      <input
+        type="text"
+        name="title"
+        placeholder="Enter Book Title"
+        required
+      />
+      <input
+        type="text"
+        name="author"
+        placeholder="Enter Author"
+        required
+      />
       <Button btnName="add-book" btnValue="Add Book" />
     </form>
   </div>
--- src/Components/App.js
+++ src/Components/App.js
@@ -2,16 +2,23 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Categories from './Categories';
 import Header from './Header';
+import AddBook from './AddBook';
 import DisplayBooks from './DisplayBooks';
 
 const App = () => (
-  <>
-    <Header />
-    <Routes>
-      <Route path="/" element={<DisplayBooks />} />
-      <Route path="/categories" element={<Categories />} />
-    </Routes>
-  </>
+  <Routes>
+    <Route
+      path="/"
+      element={(
+        <>
+          <Header />
+          <DisplayBooks />
+          <AddBook />
+        </>
+      )}
+    />
+    <Route path="/categories" element={<Categories />} />
+  </Routes>
 );
 
 export default App;
