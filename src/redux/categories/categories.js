07195ec1432dcb0c53771a4e25da259ebfceb076
Abdo Nejjari <mr.nejjari.abdelhafid@gmail.com>
2022-11-22 10:51:03 +0100

    Fix linters errors

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -10,7 +10,7 @@ export const checkBookStatus = () => ({
 });
 
 // Categories reducer
-const categoriesReducer = (state = initialState, action) =>{
+const categoriesReducer = (state = initialState, action) => {
   switch (action.type) {
     case types.CHECK_BOOK_STATUS:
       return 'Under construction';
